9f8aa439f2bfd3ef8b39881c78b79d434291905b
Nikola Kiselev <ic.nikolai.kiselev@gmail.com>
2024-07-21 06:01:50 -0400

    feat: add simple prometeus endpoint

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum"
@@ -13,6 +14,9 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ic-n/ERC4337analytics/pkg/contracts"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/urfave/cli/v2"
 )
 
@@ -27,17 +31,32 @@ var (
 		Name:    "target",
 		EnvVars: []string{"TARGET_ADDR"},
 	}
-	bundlerAddr = &cli.StringFlag{
-		Name:    "bundler",
-		EnvVars: []string{"BUNDLER_ADDR"},
+	bundlers = &cli.StringFlag{
+		Name:    "bundlers",
+		EnvVars: []string{"BUNDLERS"},
 	}
+)
+
+var (
+	opsTotal = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "user_ops_total",
+		Help: "The total number of processed events",
+	})
+	opsBundlers = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "user_ops_from_bundlers",
+		Help: "The number of processed events from bundlers",
+	})
+	opsUnknown = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "user_ops_from_unknown",
+		Help: "The number of processed events from unknown",
+	})
 )
 
 func main() {
 	app := &cli.App{
 		Name: "RC4337analytics",
 		Flags: []cli.Flag{
-			netURL, targetAddr, bundlerAddr,
+			netURL, targetAddr, bundlers,
 		},
 		Action: App,
 	}
@@ -55,11 +74,12 @@ func App(cctx *cli.Context) error {
 
 	go subscribe()
 
+	m := http.NewServeMux()
+	m.Handle("/metrics", promhttp.Handler())
+
 	s := http.Server{
-		Addr: ":80",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}),
+		Addr:              ":2112",
+		Handler:           m,
 		ReadTimeout:       time.Second * 30,
 		ReadHeaderTimeout: time.Second * 5,
 	}
@@ -110,7 +130,8 @@ func subscriber(cctx *cli.Context) (func(), error) {
 				}
 
 				log.InfoContext(ctx, fmt.Sprintf("new event: %v\n", userOperation))
-				go analyse(userOperation)
+
+				go analyse(targetEvent.Address, userOperation)
 			case <-time.After(time.Second * 3):
 				log.InfoContext(ctx, "no events")
 			}
@@ -118,10 +139,22 @@ func subscriber(cctx *cli.Context) (func(), error) {
 	}, nil
 }
 
-func analyser(cctx *cli.Context) func(userOperation *contracts.EntryPointUserOperationEvent) {
-	bundlerAddr := common.HexToAddress(bundlerAddr.Get(cctx))
+func analyser(cctx *cli.Context) func(common.Address, *contracts.EntryPointUserOperationEvent) {
+	bundlersList := strings.Split(bundlers.Get(cctx), ",")
+	bundlers := make(map[string]struct{})
+	for _, bx := range bundlersList {
+		bundlers[bx] = struct{}{}
+	}
 
-	return func(userOperation *contracts.EntryPointUserOperationEvent) {
-		_ = bundlerAddr
+	return func(address common.Address, userOperation *contracts.EntryPointUserOperationEvent) {
+		_, bundler := bundlers[address.Hex()]
+		log.InfoContext(cctx.Context, "processing event", "bundler", bundler, "userOp", userOperation)
+
+		opsTotal.Inc()
+		if bundler {
+			opsBundlers.Inc()
+			return
+		}
+		opsUnknown.Inc()
 	}
 }
